3c0b613f937da88815a8d600be9311f82861d60f
Jeff Tian <jeff.tian@outlook.com>
2023-12-21 13:17:51 +0800

    feat: parse xml

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const Koa = require('koa');
 const app = new Koa();
 const router = require('@koa/router')();
+const convert = require('xml-js');
 
 const {koaBody} = require('koa-body');
+
+app.use(koaBody({includeUnparsed: true}));
 
 router.get('/message', async (ctx, next) => {
     console.log('received message: ', ctx.query);
     ctx.body = ctx.query.echostr;
 });
 
-router.post('/message', koaBody(), async (ctx) => {
-    console.log('received message: ', ctx.request.body);
+router.post('/message', async (ctx) => {
+    const xml = ctx.request.body;
+    console.log('xml = ', xml);
+    const json = convert.xml2js(xml);
+    console.log('received message: ', json);
     ctx.body = 'ok';
 })
 
