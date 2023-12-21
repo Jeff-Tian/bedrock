790acc26f2d3e1524bbc49a339e6a52b37fbbc0b
Jeff Tian <jeff.tian@outlook.com>
2023-12-21 13:12:27 +0800

    feat: post message handler

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ router.get('/message', async (ctx, next) => {
     ctx.body = ctx.query.echostr;
 });
 
+router.post('/message', koaBody(), async (ctx) => {
+    console.log('received message: ', ctx.request.body);
+    ctx.body = 'ok';
+})
+
 
 app.use(router.routes());
 
