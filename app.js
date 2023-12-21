cee29d99f0a5d1c18f638208fdadd6a275214650
Jeff Tian <jeff.tian@outlook.com>
2023-12-21 13:11:21 +0800

    feat: get message feature

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const Koa = require('koa');
 const app = new Koa();
+const router = require('@koa/router')();
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
+const {koaBody} = require('koa-body');
+
+router.get('/message', async (ctx, next) => {
+    console.log('received message: ', ctx.query);
+    ctx.body = ctx.query.echostr;
 });
+
+
+app.use(router.routes());
 
 app.listen(3000);
